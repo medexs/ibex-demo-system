1cd1aa2c0e656cd8fa8760d11f2d4a8482fea402
Jan Medek <medekja5@fit.cvut.cz>
2024-07-30 20:27:01 +0200

    remove PWM from hello_world/main.c program

--- sw/c/demo/hello_world/main.c
+++ sw/c/demo/hello_world/main.c
@@ -6,7 +6,6 @@
 
 #include "demo_system.h"
 #include "gpio.h"
-#include "pwm.h"
 #include "timer.h"
 
 #define USE_GPIO_SHIFT_REG 0
@@ -35,13 +34,6 @@ int main(void) {
 
   // Reset green LEDs to having just one on
   set_outputs(GPIO_OUT, 0x10); // Bottom 4 bits are LCD control as you can see in top_artya7.sv
-
-  // PWM variables
-  uint32_t counter    = UINT8_MAX;
-  uint32_t brightness = 0;
-  bool ascending      = true;
-  // The three least significant bits correspond to RGB, where B is the leas significant.
-  uint8_t color = 7;
 
   while (1) {
     uint64_t cur_time = get_elapsed_time();
@@ -76,28 +68,6 @@ int main(void) {
           out_val = 0x10;
         }
         set_outputs(GPIO_OUT, out_val);
-      }
-
-      // Going from bright to dim on PWM
-      for (int i = 0; i < NUM_PWM_MODULES; i++) {
-        set_pwm(PWM_FROM_ADDR_AND_INDEX(PWM_BASE, i), ((1 << (i % 3)) & color) ? counter : 0,
-                brightness ? 1 << (brightness - 1) : 0);
-      }
-      if (ascending) {
-        brightness++;
-        if (brightness >= 5) {
-          ascending = false;
-        }
-      } else {
-        brightness--;
-        // When LEDs are off cycle through the colors
-        if (brightness == 0) {
-          ascending = true;
-          color++;
-          if (color >= 8) {
-            color = 1;
-          }
-        }
       }
     }
 

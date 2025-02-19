b00dbe6f615a9d00ac137b6ac3e41223701c64fd
Jan Medek <czmedexs@gmail.com>
2025-02-19 17:57:17 +0100

    feat(sw/c/demo/hello_world/): edit main.c for Nexys Video

--- sw/c/demo/hello_world/main.c
+++ sw/c/demo/hello_world/main.c
@@ -6,10 +6,20 @@
 
 #include "demo_system.h"
 #include "gpio.h"
-#include "pwm.h"
 #include "timer.h"
 
-#define USE_GPIO_SHIFT_REG 0
+// GPI
+#define SW0_MASK  0x1
+#define SW(n)     (SW0_MASK << n)
+#define BTNL_MASK ((SW0_MASK << 7) << 1)
+#define BTNR_MASK ((SW0_MASK << 7) << 2)
+#define BTNU_MASK ((SW0_MASK << 7) << 3)
+#define BTND_MASK ((SW0_MASK << 7) << 4)
+#define BTNC_MASK ((SW0_MASK << 7) << 5)
+
+// GPO
+#define LED0_MASK 0x1
+#define LED(n)    (LED0_MASK << n)
 
 void test_uart_irq_handler(void) __attribute__((interrupt));
 
@@ -33,15 +43,8 @@ int main(void) {
 
   uint64_t last_elapsed_time = get_elapsed_time();
 
-  // Reset green LEDs to having just one on
-  set_outputs(GPIO_OUT, 0x10); // Bottom 4 bits are LCD control as you can see in top_artya7.sv
-
-  // PWM variables
-  uint32_t counter    = UINT8_MAX;
-  uint32_t brightness = 0;
-  bool ascending      = true;
-  // The three least significant bits correspond to RGB, where B is the leas significant.
-  uint8_t color = 7;
+  // Reset LEDs to having just one on
+  set_outputs(GPIO_OUT, LED(0));
 
   while (1) {
     uint64_t cur_time = get_elapsed_time();
@@ -64,41 +67,22 @@ int main(void) {
       // Re-enable interrupts with output complete
       set_global_interrupt_enable(1);
 
-      // Cycling through green LEDs
-      if (USE_GPIO_SHIFT_REG) {
-        // Feed value of BTN0 into the shift register
-        set_outputs(GPIO_OUT_SHIFT, in_val);
-      } else {
-        // Cycle through LEDs unless BTN0 is pressed
-        uint32_t out_val = read_gpio(GPIO_OUT);
-        out_val = (out_val << 1) & GPIO_LED_MASK;
-        if ((in_val & 0x1) || (out_val == 0)) {
-          out_val = 0x10;
-        }
-        set_outputs(GPIO_OUT, out_val);
-      }
+      // Cycle through LEDs unless BTNC is pressed
+      // uint32_t out_val = read_gpio(GPIO_OUT);
+      // out_val = (out_val << 1) & 0xF;
+      // if ((in_val & BTNC_MASK) || (out_val == 0)) {
+      //   out_val = LED(0);
+      // }
+      // set_outputs(GPIO_OUT, out_val);
 
-      // Going from bright to dim on PWM
-      for (int i = 0; i < NUM_PWM_MODULES; i++) {
-        set_pwm(PWM_FROM_ADDR_AND_INDEX(PWM_BASE, i), ((1 << (i % 3)) & color) ? counter : 0,
-                brightness ? 1 << (brightness - 1) : 0);
-      }
-      if (ascending) {
-        brightness++;
-        if (brightness >= 5) {
-          ascending = false;
-        }
-      } else {
-        brightness--;
-        // When LEDs are off cycle through the colors
-        if (brightness == 0) {
-          ascending = true;
-          color++;
-          if (color >= 8) {
-            color = 1;
-          }
-        }
-      }
+      // Shift LED0 left/right based on BTNL/BTNR
+      uint32_t out_val = read_gpio(GPIO_OUT);
+      if (in_val & BTNL_MASK)
+        out_val = out_val & LED(7) ? LED(0) : out_val << 1;
+      else if (in_val & BTNR_MASK)
+        out_val = out_val & LED(0) ? LED(7) : out_val >> 1;
+
+      set_outputs(GPIO_OUT, out_val);
     }
 
     asm volatile("wfi");

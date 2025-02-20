3bb9974be7552f6d2df16e7adce72ef55ddf8880
Jan Medek <czmedexs@gmail.com>
2025-02-20 12:05:58 +0100

    fix(util/nexys-video-openocd-cfg.tcl): set ftdi channel to 1

--- util/nexys-video-openocd-cfg.tcl
+++ util/nexys-video-openocd-cfg.tcl
@@ -7,7 +7,7 @@ transport select jtag
 
 ftdi device_desc "Digilent USB Device"
 ftdi vid_pid 0x0403 0x6010
-ftdi channel 0
+ftdi channel 1
 ftdi layout_init 0x0088 0x008b
 reset_config none
 
@@ -15,7 +15,6 @@ reset_config none
 set _CHIPNAME riscv
 
 # Configure JTAG expected ID
-# basys3-xc7a35t
 set _EXPECTED_ID 0x13636093
 
 jtag newtap $_CHIPNAME cpu -irlen 6 -expected-id $_EXPECTED_ID -ignore-version
